eeb6545a03a950762447592b97ea4fb5f134e3aa
Rumsie-Grimshaw <cradleofrumsie@gmail.com>
2023-12-06 23:46:49 +0800

    Refactored code to have player races in character_creation.py to try to contain character generation to a single class.

--- character_creation.py
+++ character_creation.py
@@ -8,7 +8,7 @@ class CharacterCreation:
         player.name = input('\33[0m'"Answer: "'\33[35m')
         utility.clear()
         while not correct_name:
-            if player.name == "":
+            if player.name.strip() == "":
                 utility.clear()
                 dialogue.brewswig_no_name_given()
                 player.name = input('\33[0m'"Answer: "'\33[35m')
@@ -34,55 +34,61 @@ class CharacterCreation:
         dialogue.brewswig_tell_me_your_race(player)
         dialogue.choose_race_options()
         race = input('\33[0m'"Answer: "'\33[35m').lower()
+
         while not correct_race:
+
             if race == "i" or race == "1":
-                player.get_player_race_human(player)
+                self.get_player_race_human(player)
                 dialogue.brewswig_human_details()
                 answer = utility.confirm_option()
                 if answer == "1":
                     correct_race = True
-                else:
-                    utility.clear()
-                    correct_race = False
-                    dialogue.choose_race_options()
-                    race = input('\33[0m'"Answer: "'\33[35m').lower()
-                    continue
 
-            elif race == "ii" or race == "2":
-                player.get_player_race_elf(player)
+            if race == "ii" or race == "2":
+                self.get_player_race_elf(player)
                 dialogue.brewswig_elf_details()
                 answer = utility.confirm_option()
                 if answer == "1":
                     correct_race = True
-                else:
-                    utility.clear()
-                    correct_race = False
-                    dialogue.choose_race_options()
-                    race = input('\33[0m'"Answer: "'\33[35m').lower()
-                    continue
 
-            elif race == "iii" or race == "3":
-                player.get_player_race_dwarf(player)
+            if race == "iii" or race == "3":
+                self.get_player_race_dwarf(player)
                 dialogue.brewswig_dwarf_details()
                 answer = utility.confirm_option()
                 if answer == "1":
                     correct_race = True
-                else:
-                    utility.clear()
-                    correct_race = False
-                    dialogue.choose_race_options()
-                    race = input('\33[0m'"Answer: "'\33[35m').lower()
-                    continue
 
-            else:
-                print("\n\33[31mInvalid Selection: Please try again\n")
-                utility.press_enter_to_continue()
-                utility.clear()
-                dialogue.choose_race_options()
-                continue
+                else:
+                    print("\n\33[31mInvalid Selection: Please try again\n")
+                    utility.press_enter_to_continue()
+                    utility.clear()
+                    dialogue.choose_race_options()
+                    continue
 
     def tell_me_your_specialty(self, player):
         utility.clear()
         dialogue.brewswig_tell_me_your_specialty(player)
         input()
+
+### Character Race Options ###
+    def get_player_race_human(self, player):
+        player.race = "Human"
+        player.health = 25
+        player.strength = 10
+        player.defense = 10
+        return player.race, player.health, player.strength, player.defense
+
+    def get_player_race_elf(self, player):
+        player.race = "Elf"
+        player.health = 20
+        player.strength = 5
+        player.defense = 10
+        return player.race, player.health, player.strength, player.defense
+
+    def get_player_race_dwarf(self, player):
+        player.race = "Dwarf"
+        player.health = 30
+        player.strength = 15
+        player.defense = 10
+        return player.race, player.health, player.strength, player.defense
 
--- player.py
+++ player.py
@@ -8,35 +8,7 @@ class Player(Entity):
         super().__init__(name, race, profession, health, strength, defense)
 
 
-    def get_player_name(self):
-        valid_name = False
-        while not valid_name:
-            name = input('\33[32m'"Input your name: "'\33[0m')
-            print("")
-            if name != "":
-                return name
-            else:
-                print("Thats okay. I have plenty more glasses to clean until your ready to talk.\n")
 
-    def get_player_race_human(self, player):
-            player.race = "Human"
-            player.health = 25
-            player.strength = 10
-            player.defense = 10
-            return player.race, player.health, player.strength, player.defense
 
-    def get_player_race_elf(self, player):
-            player.race = "Elf"
-            player.health = 20
-            player.strength = 5
-            player.defense = 10
-            return player.race, player.health, player.strength, player.defense
-
-    def get_player_race_dwarf(self, player):
-            player.race = "Dwarf"
-            player.health = 30
-            player.strength = 15
-            player.defense = 10
-            return player.race, player.health, player.strength, player.defense
 
 
